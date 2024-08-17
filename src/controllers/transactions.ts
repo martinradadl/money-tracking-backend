60f53f1148df0fdaf752fc0dd70754c042d3aa0f
Martin Rada <martinradadl@gmail.com>
2024-08-17 14:58:38 -0500

    Transaction Controller functions created

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -1,12 +1,60 @@
-export const getAll = () => {
-  console.log("getTransactions");
+import { RequestHandler } from "express";
+
+const transactionModel = require("../models/transaction");
+// import transactionModel from "../models/transaction";
+
+export const getAll: RequestHandler<{}> = async (req, res) => {
+  try {
+    const transactions = await transactionModel.Transaction.find({
+      userId: req.body.userId,
+    });
+    return res.json(transactions);
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 };
-export const create = () => {
-  console.log("postTransaction");
+
+export const create: RequestHandler<{}> = async (req, res) => {
+  const newTransaction = new transactionModel.Transaction({
+    type: req.body.type,
+    concept: req.body.concept,
+    amount: req.body.amount,
+    category: req.body.category,
+    userId: req.body.userId,
+  });
+  try {
+    const createdTransaction = await newTransaction.save();
+    return res.json(createdTransaction);
+  } catch (err: any) {
+    console.log(err.message);
+    return res.send("Transaction not created");
+  }
 };
-export const edit = () => {
-  console.log("updateTransaction");
+
+export const edit: RequestHandler<{ id: string }> = async (req, res) => {
+  try {
+    const transaction = await transactionModel.Transaction.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.json(transaction);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
 };
-export const deleteMany = () => {
-  console.log("deleteManyTransaction");
+
+export const deleteMany: RequestHandler<{}> = async (req, res) => {
+  try {
+    let deletedTransaction = null;
+    for (const id of req.body.ids) {
+      deletedTransaction = await transactionModel.Transaction.findByIdAndDelete(
+        id
+      );
+    }
+    return res.json(deletedTransaction);
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
 };
