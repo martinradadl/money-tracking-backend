ac3f946953b1312026cc0108ca9c8579727fe202
Martin Rada <martinradadl@gmail.com>
2024-09-19 18:07:46 -0500

    Transactions and Users models updated

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,14 +4,12 @@ export interface UserI extends mongoose.Document {
   name: string;
   email: string;
   password: string;
-  token: string;
 }
 
 const schema = new mongoose.Schema<UserI>({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  token: String,
 });
 
 export const User = mongoose.model<UserI>("User", schema);
