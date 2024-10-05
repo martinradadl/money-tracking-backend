04d584598b9e02bc7808f1d9a0aade58d6a0c1c8
Martin Rada <martinradadl@gmail.com>
2024-10-05 18:14:17 -0500

    Currencies list and logic added

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,12 +4,14 @@ export interface UserI extends mongoose.Document {
   name: string;
   email: string;
   password: string;
+  currency: { name: string; code: string };
 }
 
 const schema = new mongoose.Schema<UserI>({
   name: String,
   email: { type: String, unique: true },
   password: String,
+  currency: { name: String, code: String },
 });
 
 export const User = mongoose.model<UserI>("User", schema);
