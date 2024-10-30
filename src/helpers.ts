eda89dc0aadff19583d3e864e5a115817ba4dfa7
Martin Rada <martinradadl@gmail.com>
2024-10-30 11:29:47 -0500

    NODE_ENV logic updated

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
 export const APP_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.APP_DEV_URL
-    : process.env.APP_URL;
+  process.env.NODE_ENV === "production"
+    ? process.env.APP_URL
+    : process.env.APP_DEV_URL;
