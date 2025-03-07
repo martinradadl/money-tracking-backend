86ade29a0f101ff815676034542464e33b266e8f
Martin Rada <martinradadl@gmail.com>
2025-03-07 17:32:16 -0500

    New routes added

--- src/routes/debts.ts
+++ src/routes/debts.ts
@@ -6,18 +6,48 @@ const router = express.Router();
 
 router.get("/balance/:userId", tokenVerification, debtsController.getBalance);
 router.get(
-    "/balance/loans/:userId",
-    tokenVerification,
-    debtsController.getTotalLoans
-  );
-  router.get(
-    "/balance/debts/:userId",
-    tokenVerification,
-    debtsController.getTotalDebts
-  );
+  "/balance/loans/:userId",
+  tokenVerification,
+  debtsController.getTotalLoans
+);
+router.get(
+  "/balance/debts/:userId",
+  tokenVerification,
+  debtsController.getTotalDebts
+);
 router.get("/:userId", tokenVerification, debtsController.getAll);
 router.post("/", tokenVerification, debtsController.create);
 router.put("/:id", tokenVerification, debtsController.edit);
 router.delete("/:id", tokenVerification, debtsController.deleteOne);
+router.get(
+  "/:userId/filter/day",
+  tokenVerification,
+  debtsController.filterByDay
+);
+router.get(
+  "/:userId/filter/month",
+  tokenVerification,
+  debtsController.filterByMonth
+);
+router.get(
+  "/:userId/filter/year",
+  tokenVerification,
+  debtsController.filterByYear
+);
+router.get(
+  "/:userId/filter/custom-days",
+  tokenVerification,
+  debtsController.filterByCustomDays
+);
+router.get(
+  "/:userId/filter/custom-months",
+  tokenVerification,
+  debtsController.filterByCustomMonths
+);
+router.get(
+  "/:userId/filter/custom-years",
+  tokenVerification,
+  debtsController.filterByCustomYears
+);
 
 export default router;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -23,5 +23,35 @@ router.get("/:userId", tokenVerification, transactionsController.getAll);
 router.post("/", tokenVerification, transactionsController.create);
 router.put("/:id", tokenVerification, transactionsController.edit);
 router.delete("/:id", tokenVerification, transactionsController.deleteOne);
+router.get(
+  "/:userId/filter/day",
+  tokenVerification,
+  transactionsController.filterByDay
+);
+router.get(
+  "/:userId/filter/month",
+  tokenVerification,
+  transactionsController.filterByMonth
+);
+router.get(
+  "/:userId/filter/year",
+  tokenVerification,
+  transactionsController.filterByYear
+);
+router.get(
+  "/:userId/filter/custom-days",
+  tokenVerification,
+  transactionsController.filterByCustomDays
+);
+router.get(
+  "/:userId/filter/custom-months",
+  tokenVerification,
+  transactionsController.filterByCustomMonths
+);
+router.get(
+  "/:userId/filter/custom-years",
+  tokenVerification,
+  transactionsController.filterByCustomYears
+);
 
 export default router;
