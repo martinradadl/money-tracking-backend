cdc39b1fe45a2af233c12ef6dc0fdbef69fd1591
Martin Rada <martinradadl@gmail.com>
2025-03-29 19:06:54 -0500

    Movements helpers updated, incluiding tests

--- src/helpers/movements.ts
+++ src/helpers/movements.ts
@@ -1,18 +1,54 @@
 import { addDays, addMonths, addYears } from "date-fns";
 
-export const getStartAndEndDates = (
-  timePeriod: string,
-  selectedDate?: string,
-  selectedStartDate?: string,
-  selectedEndDate?: string
-) => {
-  const dayStartDate = new Date(`${selectedDate}T00:00:00.000+00:00`);
-  const monthStartDate = new Date(`${selectedDate}-01T00:00:00.000+00:00`);
-  const yearStartDate = new Date(`${selectedDate}-01-01T00:00:00.000+00:00`);
+export const movementsErrors = {
+  noDates: new Error(
+    "This method requires at least a selectedDate or a selectedStartDate and selectedEndDate"
+  ),
+  incompleteDateRange: new Error(
+    "This method requires both a selectedStartDate and selectedEndDate"
+  ),
+  swappedDateRange: new Error(
+    "selectedStartDate must be earlier than selectedEndDate"
+  ),
+};
 
+type getStartAndEndDatesParams = {
+  timePeriod: string;
+  selectedDate?: string;
+  selectedStartDate?: string;
+  selectedEndDate?: string;
+};
+
+export const getStartAndEndDates = (params: getStartAndEndDatesParams) => {
   type TimePeriods = { [key: string]: { startDate: Date; endDate: Date } };
   let timePeriods: TimePeriods = {};
-  if (selectedDate) {
+  let error = null;
+
+  if (
+    !params.selectedDate &&
+    !params.selectedStartDate &&
+    !params.selectedEndDate
+  ) {
+    error = movementsErrors.noDates;
+  } else if (
+    !params.selectedDate &&
+    !(params.selectedStartDate && params.selectedEndDate)
+  ) {
+    error = movementsErrors.incompleteDateRange;
+  } else if (
+    params.selectedStartDate &&
+    params.selectedEndDate &&
+    new Date(params.selectedEndDate) < new Date(params.selectedStartDate)
+  ) {
+    error = movementsErrors.swappedDateRange;
+  } else if (params.selectedDate) {
+    const dayStartDate = new Date(`${params.selectedDate}T00:00:00.000+00:00`);
+    const monthStartDate = new Date(
+      `${params.selectedDate}-01T00:00:00.000+00:00`
+    );
+    const yearStartDate = new Date(
+      `${params.selectedDate}-01-01T00:00:00.000+00:00`
+    );
     timePeriods = {
       day: {
         startDate: dayStartDate,
@@ -30,24 +66,31 @@ export const getStartAndEndDates = (
   } else {
     timePeriods = {
       day: {
-        startDate: new Date(`${selectedStartDate}T00:00:00.000+00:00`),
-        endDate: addDays(new Date(`${selectedEndDate}T00:00:00.000+00:00`), 1),
+        startDate: new Date(`${params.selectedStartDate}T00:00:00.000+00:00`),
+        endDate: addDays(
+          new Date(`${params.selectedEndDate}T00:00:00.000+00:00`),
+          1
+        ),
       },
       month: {
-        startDate: new Date(`${selectedStartDate}-01T00:00:00.000+00:00`),
+        startDate: new Date(
+          `${params.selectedStartDate}-01T00:00:00.000+00:00`
+        ),
         endDate: addMonths(
-          new Date(`${selectedEndDate}-01T00:00:00.000+00:00`),
+          new Date(`${params.selectedEndDate}-01T00:00:00.000+00:00`),
           1
         ),
       },
       year: {
-        startDate: new Date(`${selectedStartDate}-01-01T00:00:00.000+00:00`),
+        startDate: new Date(
+          `${params.selectedStartDate}-01-01T00:00:00.000+00:00`
+        ),
         endDate: addYears(
-          new Date(`${selectedEndDate}-01-01T00:00:00.000+00:00`),
+          new Date(`${params.selectedEndDate}-01-01T00:00:00.000+00:00`),
           1
         ),
       },
     };
   }
-  return timePeriods[timePeriod];
+  return { error, data: timePeriods[params.timePeriod] };
 };
